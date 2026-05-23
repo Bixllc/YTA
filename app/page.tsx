1dfbdae3ea770c1c9625fb5e7a0c938e2abc5275
Sheneska <sheneska101@gmail.com>
2026-05-23 16:49:33 -0400

    feat: wire all sections into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
+import RosterSection from "@/components/RosterSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <RosterSection />
+      <ContactSection />
+      <Footer />
     </main>
   );
 }
